16517e60124077f35986fee43f6cc7fa070297a7
allai5 <alicelai4396@gmail.cpm>
2019-01-15 18:09:35 -0500

    1st version of motors API

--- motors.py
+++ motors.py
@@ -1,6 +1,12 @@
 import time
 from adafruit_motorkit import MotorKit
 
+'''
+TO-DO LIST:
+Fix motor numbers for functions
+Fix negative numbers (CCW vs CW) for throttle values
+
+'''
 kit = MotorKit()
 
 
@@ -22,18 +28,43 @@ def test_motors():
     time.sleep(0.5)
     kit.motor4.throttle = 0
 
+def reset_motors():
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    kit.motor3.throttle = 0
+    kit.motor4.throttle = 0
+
+def left_motors(left_delay, left_throttle):
+    kit.motor1.throttle = left_throttle
+    kit.motor3.throttle = left_throttle
+    time.sleep(left_delay)
+
+def right_motors(right_delay, right_throttle):
+    kit.motor2.throttle = right_throttle
+    kit.motor4.throttle = right_throttle
+    time.sleep(right_delay)
+
 #robot forward
-def forward():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = 1.0
-    time.sleep(0.5)
+def forward(delay, throttle):    
+    left_motors(delay, -throttle)
+    right_motors(delay, throttle)
+    reset_motors()
 
 #robot backward
+def backward(delay, throttle):
+    left_motors(delay, throttle)
+    right_motors(delay, -throttle)
+    reset_motors()
 
 #robot CCW turn
+def turn_CCW(delay, throttle):
+    left_motors(delay, throttle)
+    right_motors(delay, throttle)
+    reset_motors()
 
 #robot CW turn
-
-
-
+def turn_CW(delay, throttle):
+    left_motors(delay, -throttle)
+    right_motors(delay, -throttle)
+    reset_motors()
 
