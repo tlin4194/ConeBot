e064661e7a47ba8e9f80c6f1a30096437b8b9ce1
allai5 <alicelai4396@gmail.com>
2019-01-17 18:33:30 -0500

    numbers for motors correct now

--- motors.py
+++ motors.py
@@ -5,7 +5,12 @@ from adafruit_motorkit import MotorKit
 TO-DO LIST:
 Fix motor numbers for functions
 Fix negative numbers (CCW vs CW) for throttle values
+A = 1
+B = 2
+C = 3
+D = 4
 
+Odd numbered motors are in the front, even numbered in the back
 '''
 kit = MotorKit()
 
@@ -30,13 +35,13 @@ def reset_motors():
     kit.motor4.throttle = 0
 
 def left_motors(left_delay, left_throttle):
-    kit.motor1.throttle = left_throttle
+    kit.motor4.throttle = left_throttle
     kit.motor3.throttle = left_throttle
     time.sleep(left_delay)
 
 def right_motors(right_delay, right_throttle):
     kit.motor2.throttle = right_throttle
-    kit.motor4.throttle = right_throttle
+    kit.motor1.throttle = right_throttle
     time.sleep(right_delay)
 
 
