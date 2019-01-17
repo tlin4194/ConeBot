9acc8c737dcd73f0400eb66b487ec1597cedb4ee
amnayak <amkt8@yahoo.com>
2019-01-16 22:13:54 -0500

    fixed client

--- client.py
+++ client.py
@@ -2,19 +2,17 @@ import requests
 import readline
 
 print('Clientside script running')
-user_input = ''
+user_input = ' '
 
-while (user_input != 'q'):
+while (user_input[0] != 'q'):
     r = 'invalid command'
-    user_input = raw_input('Command:')
-    switch(user_input) {
-        case 'h':
+    user_input = ' '
+    user_input = raw_input('Command:')[0]
+    if (user_input == 'h'):
         r = requests.get('http://conebot.wv.cc.cmu.edu:5000/')
-        case 't':
+    if (user_input == 't'):
         r = requests.get('http://conebot.wv.cc.cmu.edu:5000/test')
-    }
-    print(r.json() + '\n')
-    user_input = ''
+    print(r + '\n')
 
 
 print('---------------')
